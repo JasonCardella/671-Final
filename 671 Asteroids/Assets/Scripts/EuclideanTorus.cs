c37f4b5a03227d0bf51ce26afdc5e8df33e28867
JasonCardella <jec4762@rit.edu>
2021-03-27 15:20:21 -0400

    First Push
    Uploading project. Includes: Documentation, Reaper Project, FMOD Project, Unity Project

---
+++ 671 Asteroids/Assets/Scripts/EuclideanTorus.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EuclideanTorus : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //Wrap game object
+        if (transform.position.x > 9)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(9, transform.position.y, 0);
+        }
+        else if (transform.position.y > 6)
+        {
+            transform.position = new Vector3(transform.position.x, -6, 0);
+        }
+        else if (transform.position.y < -6)
+        {
+            transform.position = new Vector3(transform.position.x, 6, 0);
+        }
+
+    }
+}
